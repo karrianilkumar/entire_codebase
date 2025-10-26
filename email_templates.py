8bc11700c0c975380b130a72d33c5e98e18d3b77
karrianilkumar <162177384+karrianilkumar@users.noreply.github.com>
2025-10-26 16:56:01 +0530

    Create email_templates.py

---
+++ email_templates.py
@@ -0,0 +1,19 @@
+Hi [Name],
+Good morning! Hope you’re doing well.
+
+I’m Anil Kumar Karri, a Python Backend Intern at Algonox Technologies, Hyderabad, currently working on a large-scale SIM Management Platform (AMOP) using Python, Flask, PostgreSQL, and AWS.
+
+I developed projects like AI-Powered YouTube Notes Generator, a web app that summarizes YouTube videos and translates them using Google APIs. I also built an HR Helper Bot that retrieves employee data using NLP queries, and a Cataract Diagnosis System that leverages ML for medical predictions. These projects reflect my ability to apply Python, Flask, and ML/NLP to build scalable and impactful solutions.
+
+I believe an entry-level backend or AI developer role at your organization would be a great opportunity to learn and contribute meaningfully. I would be truly grateful if you could refer me for any suitable opportunities or guide me on how to proceed further.
+
+Thank you for your time and support. Looking forward to your response.
+
+Best regards,
+Anil Kumar Karri
+📞 9391687847 | 📧 karrianilkumar101@gmail.com
+
+🔗 LinkedIn
+ | GitHub
+
+============================================================>
